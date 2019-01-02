7d83a9f398a08fb2b1d8a7ff996ec9fbb42b3c33
anbang <79228807@qq.com>
2019-01-02 11:32:49 +0800

    MODI

--- down.js
+++ down.js
@@ -1,17 +1,13 @@
 let fs = require('fs');
 let http = require("http");
-let https = require("https");
 const path = require('path');
 const decompress = require('decompress');
 
 let process, percent;
-let req, flag = 0, globalRes, isStopDown = false;
+let req, flag = 0, globalRes;
 let timer=null;
 
 let url = 'http://nodejs.org/dist/v10.15.0/node-v10.15.0-win-x64.zip';
-let url1 = 'http://www.canonchain.com/resource/file/canonchain/latest/builds/canonchain-0.8.1.tar.gz';//win
-let url2 = 'http://www.canonchain.com/resource/file/canonchain/latest/builds/canonchain-windows-amd64-0.8.1.zip';//MAC
-let url3= 'https://canonchain-public.oss-cn-hangzhou.aliyuncs.com/node/mac/canonchain-lastest.tar.gz';
 let filePath = __dirname;
 const zipFilePath = path.join(filePath, 'test.zip')
 
@@ -26,15 +22,14 @@ function down() {
             //相同，有事情了；
             console.log("ERROR 出大事了,可能下载到99%卡住了,要回炉再造了！！！！！！！！！！！！！！！！！！！！！")
             req = null;
-            isStopDown = true;
             clearInterval(timer);
             timer=null;
-            globalRes.emit('end');
+            globalRes.emit('destroy');
             down();
         }
     }, 3000)
 
-    req = http.get(url1, (res) => {
+    req = http.get(url, (res) => {
         let canonData = "";
         globalRes = res;
         res.setEncoding("binary");//binary
@@ -43,30 +38,30 @@ function down() {
         console.log("=========================================================")
         res.on("data", (chunk) => {
             canonData += chunk;
-            isStopDown = false;
             writeStream.write(chunk)
             process = ((canonData.length) / contentLength) * 100;
             percent = parseInt(Math.floor(process).toFixed(0));
             //任务栏进度条
             console.log(`${percent}% => ${process.toFixed(3)}`);
+            // globalRes.destroy();
         });
         res.on("end", () => {
             canonData = "";
             writeStream.end();
             console.log("+++++++++++++++++++++++++++++++End")
         });
+        res.on('destroy',()=>{
+            console.log("download destroy",canonData.length)
+        })
         //结束后，解压
         writeStream.on('finish', () => {
-            if (!isStopDown) {
-                clearInterval(timer);
-                timer=null;
-                decompress(zipFilePath, filePath).then(files => console.log('decompress end [全部完成]'))
-            }
-
+            clearInterval(timer);
+            timer=null;
+            decompress(zipFilePath, filePath).then(files => console.log('decompress end [全部完成]'))
         })
     });
 
-    //下载canonchain出错
+    //Error
     req.on('error', (e) => {
         console.error('problem with request: ' + e.message);
     });
